511f2c13a7cc5f95b9711e1149f4b7c30f199507
BalyRo <roibaly14@gmail.com>
2026-04-20 10:32:10 +0300

    initial commit for exercise

---
+++ RoiBaly.java
@@ -0,0 +1,7 @@
+public class RoiBaly {
+    public static void main(String[] args) {
+        for (int i =0;i<=5;i++){
+            System.out.println(i+ "my name is Roi Baly");
+        }
+    }
+}
