422068d281768023dc8ec820f45f69369dffaed4
BalyRo <roibaly14@gmail.com>
2026-04-20 11:12:12 +0300

    updated hello message

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
         int i;
         for (i = 1; i <= 10; i++)
         {
-            System.out.println("Hello World..."+ i );
+            System.out.println("Not Hello World..."+ i );
             System.out.println("things has change now.....");
         }
     }
